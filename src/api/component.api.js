c559f19122a2344860dced2a898843de3613a957
unknown <vinicius.marconcini@gmail.com>
2019-04-20 15:01:53 -0300

    Continuation of registers

--- src/api/component.api.js
+++ src/api/component.api.js
@@ -2,13 +2,25 @@ import express from 'express';
 
 import { DefaultController } from '../controllers/default.controller';
 import { Component } from '../../server/models/';
+import ComponentController from '../controllers/component.controller';
 
 const componentApi = express();
 
-const componentController = new DefaultController(Component);
+const componentControllerDefault = new DefaultController(Component);
+
+componentApi.post('/create', async (req, res) => {
+  const { componentName, categoryId } = req.body;
+
+  await ComponentController.create({
+    nm_component: componentName,
+    id_category: categoryId
+  });
+
+  res.status(200).send();
+});
 
 componentApi.get('/', (req, res) => {
-  componentController.list(req, res, ['nm_component']);
+  componentControllerDefault.list(req, res, ['nm_component']);
 });
 
 export default componentApi;
